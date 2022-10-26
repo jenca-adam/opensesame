49bd14f98fedb44b87b5e15334c7fb1daa01722b
Adam Jenča <jenca.adam@gmail.com>
2022-10-26 20:16:13 +0200

    i

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+echo "Setting up..."
+VERSION=$(python --version |awk --field-separator=" " '{ print $2}'|awk -F. '{print $1"."$2}')
+if [ "$?" == "127" ]
+        then
+	echo "Python 3 not found! please install it."
+	exit 1
+   fi
+echo "Python version is $VERSION"
+echo "Trying setup.py install..."
+sudo python3 setup.py install 2>>/dev/null
+if [ "$?" == "1" ]
+then
+echo "No root! Installing locally."
+python3 setup.py install --prefix="~/.local/"
+fi
